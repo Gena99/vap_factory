6ab5d3661bf4cc8fb2219281e33b96781099669d
gena99 <gena@e64.net>
2022-01-09 19:13:10 +0100

    CRUD ok

--- index.php
+++ index.php
@@ -8,11 +8,21 @@ try {
     die('Erreur : ' . $e->getMessage());
 }
 
-$select = 'SELECT * from vap_factory.produits';
-$rows = $db->query($select);
-$produit = $rows->fetchAll();
-var_dump($produit);
+$selectAll = 'SELECT * from vap_factory.produits';
+$rows = $db->query($selectAll);
+$produits = $rows->fetchAll();
+var_dump($produits);
 
 $create ="INSERT INTO `vap_factory`.`produits` (`Reference`, `Nom`, `Description`, `Prix_achat_unitaire`, `Prix_vente_unitaire`) VALUES ('10', 'blabla', 'pfff', '8', '0')";
 $db->exec($create);
+// mis à jour d'un produit
+$update = "UPDATE `vap_factory`.`produits` SET `Quantite_stock`='24' WHERE reference ='10' ";
+$db->exec($update);
 
+$select = "SELECT * FROM vap_factory.produits WHERE reference = '10';";
+$statment =$db->query($select);
+$produit = $statment->fetchAll();
+var_dump($produit);
+
+$delete ="DELETE FROM `vap_factory`.`produits` WHERE (`Reference` = '10');";
+$db->exec($delete);
